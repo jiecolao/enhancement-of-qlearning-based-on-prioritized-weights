140f0020f514b5dfbb922a9c31eec493601528e7
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-09-05 22:50:57 +0800

    feat: added episode and interval properties

--- src/EQLBPW/tracker.py
+++ src/EQLBPW/tracker.py
@@ -33,6 +33,12 @@ class EnvironmentTracker:
         self.steps_history = []
         self.optimality_history = []
 
+        self.episode_steps = 0
+        self.episode_reward = 0
+
+        self.interval_steps = 0
+        self.interval_reward = 0
+
         self.shortest_path = self.calculate_shortest_path()
 
         if self.shortest_path is not None:
@@ -145,22 +151,27 @@ class EnvironmentTracker:
         return None
     
     def record_episode(self, success):
-        self.steps_history.append(self.steps_per_ep)
-        self.reward_history.append(self.rewards_per_ep)
-
+        self.steps_history.append(self.episode_steps)
+        self.reward_history.append(self.episode_reward)
         self.success_history.append(int(success))
 
         if success:
             self.successes_per_ep += 1
-            self.path_lengths.append(self.steps_per_ep)
+            self.path_lengths.append(self.episode_steps)
 
             if self.shortest_path_steps is not None:
                 optimality = (
-                    self.shortest_path_steps / self.steps_per_ep
-                    if self.steps_per_ep > 0
+                    self.shortest_path_steps / self.episode_steps
+                    if self.episode_steps > 0
                     else 0.0
                 )
                 self.optimality_history.append(optimality)
+
+        self.interval_steps += self.episode_steps
+        self.interval_reward += self.episode_reward
+
+        self.episode_steps = 0
+        self.episode_reward = 0
 
     def get_success_rate(self):
         if not self.success_history:
