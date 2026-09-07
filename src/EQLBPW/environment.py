e4af92994c99c903784453a6706c2aa8aae632fe
Jieco Lao <jiecoandres.lao07@gmail.com>
2026-09-07 09:24:10 +0800

    fix: resolved max_step of EQLBPW

--- src/EQLBPW/environment.py
+++ src/EQLBPW/environment.py
@@ -37,7 +37,7 @@ class Environment:
 
         self.agent_pos = start_state
         self.steps = 0
-        self.max_steps = self.grid_cols * self.grid_cols
+        self.max_steps = self.grid_cols * self.grid_cols ** 2
 
         self.tracker = EnvironmentTracker(
             agent=agent,
